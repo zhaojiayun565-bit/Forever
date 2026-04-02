a09b57421a331b725afff131952f5552fbd0a6dc
Jia Yun Zhao <zhaojiayun565@gmail.com>
2026-04-02 17:28:17 -0400

    Initial Commit

---
+++ Forever/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Forever
+//
+//  Created by Jia Yun Zhao on 2026-04-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Forever/ForeverApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForeverApp.swift
+//  Forever
+//
+//  Created by Jia Yun Zhao on 2026-04-02.
+//
+
+import SwiftUI
+
+@main
+struct ForeverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
